0e3dd55f21d6fa1b66ee85d5bc3b1e232abcf97b
DGS-29 <deepakgsarode09@gmail.com>
2024-07-01 16:25:48 +0530

    Amazon Clone

--- lib/features/auth/services/auth_service.dart
+++ lib/features/auth/services/auth_service.dart
@@ -11,72 +11,97 @@ import 'package:http/http.dart' as http;
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-class AuthService{
-
+class AuthService {
   // sign up user
-  void signUpUser({
+  Future<void> signUpUser({
     required BuildContext context,
     required String email,
     required String password,
     required String name,
-  })async{
-    try{ 
+  }) async {
+    try {
       User user = User(
-        id: '', 
+        id: '',
         name: name,
-        password: password, 
-        address: '', 
-        type: '', 
-        token: '', email: email,
-        );
+        password: password,
+        address: '',
+        type: '',
+        token: '',
+        email: email,
+      );
 
-      http.Response res = await http.post(Uri.parse('$uri/api/signup'), 
-      body: user.toJson(),
-      headers: <String, String>{
-        'Content-Type': 'application/json; charset=UTF-8',
-      },
-       );
-      // ignore: use_build_context_synchronously
-      httpErrorHandle(response: res, context: context, onSuccess: (){
-        showSnackBar(context, 'Account created! Login with the same credentials!',
-        );
-      },
+      http.Response res = await http.post(
+        Uri.parse('$uri/api/signup'),
+        body: user.toJson(),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+      );
+
+      if (!context.mounted) return;
+      httpErrorHandle(
+        response: res,
+        context: context,
+        onSuccess: () {
+          showSnackBar(
+            context,
+            'Account created! Login with the same credentials!',
+          );
+        },
       );
     } catch (e) {
-      // ignore: use_build_context_synchronously
-      showSnackBar(context, e.toString());
+      if (context.mounted) {
+        showSnackBar(context, e.toString());
+      }
     }
   }
-  //sign In user
-   void signInUser({
+
+  // sign in user
+  Future<void> signInUser({
     required BuildContext context,
     required String email,
     required String password,
-  })async{
-    try{ 
-      http.Response res = await http.post(Uri.parse('$uri/api/signin'), 
-      body: jsonEncode({
-        'email': email,
-        'password': password,
-      }),
-      headers: <String, String>{
-        'Content-Type': 'application/json; charset=UTF-8',
-      }
-       );
+  }) async {
+    try {
+      http.Response res = await http.post(
+        Uri.parse('$uri/api/signin'),
+        body: jsonEncode({
+          'email': email,
+          'password': password,
+        }),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+      );
 
       print('Response: ${res.body}');
-        httpErrorHandle(response: res, context: context, onSuccess: () async {
-        SharedPreferences prefs = await SharedPreferences.getInstance();
 
-        Provider.of<UserProvider>(context, listen: false).setUser(res.body);
-        await prefs.setString('x-auth-token', jsonDecode(res.body)['token']);
+      if (!context.mounted) return;
+      final userProvider = Provider.of<UserProvider>(context, listen: false);
 
-        Navigator.pushAndRemoveUntil(context, HomeScreen.routeName as Route<Object?>, (route) => false,);
-      },
+      httpErrorHandle(
+        response: res,
+        context: context,
+        onSuccess: () async {
+          SharedPreferences prefs = await SharedPreferences.getInstance();
+          userProvider.setUser(res.body);
+          await prefs.setString('x-auth-token', jsonDecode(res.body)['token']);
+
+          if (context.mounted) {
+            Navigator.pushAndRemoveUntil(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const HomeScreen(),
+              ),
+              (route) => false,
+            );
+          }
+        },
       );
     } catch (e) {
-
-      showSnackBar(context, e.toString());
+      if (context.mounted) {
+        showSnackBar(context, e.toString());
+      }
     }
   }
 }
