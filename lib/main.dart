d0f02095bc318efd598534abf9d80254e24aa2ac
DGS-29 <deepakgsarode09@gmail.com>
2024-08-07 01:36:16 +0530

    Amazon clone

--- lib/main.dart
+++ lib/main.dart
@@ -1,63 +1,56 @@
-// ignore_for_file: unnecessary_const
-
-// ignore: unused_import
+import 'package:amazon_clone/common/widgets/bottom_bar.dart';
+import 'package:amazon_clone/common/widgets/admin_bottom_bar.dart';
 import 'package:amazon_clone/features/auth/screens/auth_screen.dart';
+import 'package:amazon_clone/features/auth/services/auth_service.dart';
 import 'package:amazon_clone/providers/user_provider.dart';
 import 'package:amazon_clone/router.dart';
 import 'package:flutter/material.dart';
+import 'package:amazon_clone/constants/global_variables.dart';
 import 'package:provider/provider.dart';
 
 void main() {
-  runApp(MultiProvider(providers: [
-    ChangeNotifierProvider(create: (context) => UserProvider(),),
-  ] ,child: const MyApp()));
+  runApp(MultiProvider(providers:[
+    ChangeNotifierProvider(create: (context)=> UserProvider(),)
+  ],
+  child: const MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final AuthService authService = AuthService();
+
+  @override
+  void initState() {
+    // get user data function
+    super.initState();
+    authService.getUserData(context: context);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Amazon Clone',
-      theme: ThemeData(
-        scaffoldBackgroundColor: const Color.fromARGB(255, 224, 249, 144),
-        colorScheme: const ColorScheme.light(
-          primary: Color.fromARGB(255, 255, 0, 221),
-        ),
-        appBarTheme: const AppBarTheme(
-          elevation: 0,
-          iconTheme: IconThemeData(
-            color: Colors.black26,
+        title: "Amazon Clone",
+        theme: ThemeData(
+          scaffoldBackgroundColor: GlobalVariables.backgroundColor,
+          colorScheme:
+              const ColorScheme.light(primary: GlobalVariables.secondaryColor),
+          appBarTheme: const AppBarTheme(
+            elevation: 0,
+            iconTheme: IconThemeData (color: Colors.black),
+            backgroundColor: GlobalVariables.secondaryColor,
+            foregroundColor: Colors.white,
           ),
         ),
-      ),
-      onGenerateRoute: (settings) => generateRoute(settings),
-      home: const HomeScreen(),
-    );
-  }
-}
-
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const  Center(child: Text('Hey.')),
-        centerTitle: true,
-        backgroundColor: Colors.amber,
-      ), 
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pushNamed(context, AuthScreen.routeName);
-          },
-          child: const Text('Go to Auth Screen'),
-        ),
-      ),
+        onGenerateRoute: (settings) => generateRoute(settings),
+        home: Provider.of<UserProvider>(context).user.token.isNotEmpty 
+        ? Provider.of<UserProvider>(context).user.type=='user' ? const BottomBar() : const AdminBottomBar()
+        : const AuthScreen()
     );
   }
 }
